b5a3d2ba40c18810269d2b0735a17c56b0f1ab14
Любовь Сергеевна Анохина <Anokhina_l@AZARD.local>
2024-10-09 11:08:17 +0300

    initial commit

---
+++ src/components/TheFooter.vue
@@ -0,0 +1,14 @@
+<template>
+    <footer class="footer">
+        <div class="container">
+                <ul class="navbar-list">
+                    <li class="navbar-item">
+                        <a href="/" class="navbar-link">Home</a>
+                    </li>
+                    <li class="navbar-item">
+                        <a href="/about" class="navbar-link">About</a>
+                    </li>
+                </ul>
+        </div>
+    </footer>
+</template>
